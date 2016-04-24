23c71539d47381cc7abdf95b5bbb53048474f70c
yuxuanx <xiayuxuan1993@outlook.com>
2016-04-24 21:45:24 +0200

    minor changes

--- HA3_1.m
+++ HA3_1.m
@@ -1,9 +1,9 @@
 theta = 0:0.01:6;
-y1 = (theta-3).^2;
-y2 = (theta-1).^2;
+y1 = 1/4*(theta-3).^2 + 1/4;
+y2 = 1/121*(theta-1).^2 + 100/121;
 y3 = ones(length(theta),1)';
 figure;
 plot(theta,[y1;y2;y3]);
 xlabel('\theta');
 legend('R(\theta,y_1(x)=3)','R(\theta,y_2(x)=1)','R(\theta,y(x)=x)');
-axis([0 6 0 6]);
+%axis([0 6 0 6]);
